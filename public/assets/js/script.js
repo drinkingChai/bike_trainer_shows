b9776d96e4f32dc14401f52a4cd5c4944b000b6e
Wasif Zaman <teatocode@gmail.com>
2016-12-29 07:50:11 -0500

    Change sort button to close button when sort is open

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,21 +1,15 @@
 $('.overlay').hide();
+$('#sort-close').hide();
 
-$("#sort-button, .overlay").click(function() {
+$("#sort-open, #sort-close, .overlay").click(function() {
   var $sortFilter = $('.sort-filter > .inner');
   if ($sortFilter.hasClass('right-zero')) {
     $('.sort-filter > .inner').css('right', '-65%');
   } else {
     $('.sort-filter > .inner').css('right', 0);
   }
+  $('#sort-close, #sort-open').toggle();
   $('.sort-filter > .inner').toggleClass('right-zero');
   $('.overlay').toggle();
   $('.movie-cards .inner').toggleClass('prevent-scroll');
 })
-
-// $("#sort-button").toggle(
-//   function() {
-//     $('.sort-filter > .inner').css('right', 0);
-//   },
-//   function() {
-//     $('.sort-filter > .inner').css('right', '-50%');
-//   })
