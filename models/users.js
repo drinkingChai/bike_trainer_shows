4cb437152c7046b0e30d747db94a72017df1790e
Wasif Zaman <teatocode@gmail.com>
2017-01-04 01:45:47 -0500

    Add user to backend

--- models/users.js
+++ models/users.js
@@ -4,31 +4,36 @@ var parseUrlJSON = bodyParser.json();
 var jwt = require('jsonwebtoken');
 var expressJwt = require('express-jwt');
 var MongoClient = require('mongodb').MongoClient;
+var statics = require('./statics');
 
-var jwtSecret = 'oi1q3h4ropiu(*P#240u09)';
 
 var url = 'mongodb://localhost:27017/bike_trainer_shows';
 
 var users = express();
 
 users.use(parseUrlJSON);
-users.use(expressJwt({ secret: jwtSecret }).unless({ path: ['/users/login', '/users/new'] }));
+users.use(expressJwt({ secret: statics.jwtSecret }).unless({ path: ['/users/login', '/users/new'] }));
 
-var user = {};
 
 users.get('/', function(request, response) {
   response.sendStatus(200);
 })
 
 users.post('/login', authenticate, function(request, response) {
-  // MongoDb get user
-  var token = jwt.sign({
-    username: user.username,
-  }, jwtSecret);
-  response.send({
-    user: user,
-    token: token
-  });
+  var body = request.body;
+  MongoClient.connect(url, function(err, db) {
+    db.collection('users').findOne({
+      username: body.username
+    }, function(err, result) {
+      var token = jwt.sign({
+        username: result.username
+      }, statics.jwtSecret);
+      response.send({
+        user: result,
+        token: token
+      })
+    })
+  })
 })
 
 users.post('/new', function(request, response) {
@@ -49,7 +54,7 @@ users.post('/new', function(request, response) {
 
 users.get('/me', function(request, response) {
   MongoClient.connect(url, function(err, db) {
-    db.collection('users').findOne({ username: 'test' }, function(err, result) {
+    db.collection('users').findOne({ username: request.user.username }, function(err, result) {
       request.user.movies = result.movies;
       response.send(request.user);
     })
@@ -61,10 +66,6 @@ function authenticate(request, response, next) {
   if (!body.username || !body.password) {
     response.status(400).end('empty_field');
   }
-  // if (body.username !== user.username || body.password != user.password) {
-  //   response.status(401).end('wrong_userpass');
-  // }
-  // check against MongoDB user database
   MongoClient.connect(url, function(err, db) {
     db.collection('users').findOne({ username: body.username }, function(err, result) {
       if (result.password === body.password) {
